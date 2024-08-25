c1991164cb5af67e568fe8ed844a7d0b05b2218d
Clement Adetunji <clemxytech@gmail.com>
2024-08-25 19:50:14 +0100

    Update rps_python.py

--- rps_python.py
+++ rps_python.py
@@ -29,21 +29,30 @@ scissors = '''
 rps = [rock, paper, scissors]
 print("What do you choose? Type 0 for Rock, 1 for Paper or 2 for Scissors. \n")
 
-#Player decision
-player_choice = rps[int(input())]
-print(player_choice)
+player = int(input())
+if player >= 0 and player < 3:
+    # Player decision
+    player_choice = rps[player]
+    print(player_choice)
 
-#Computer decision
-computer_choice = rps[random.randint(0,2)]
-print(f"Computer Chose: \n {computer_choice}")
+    # Computer decision
+    computer_choice = rps[random.randint(0, 2)]
+    print(f"Computer Chose: \n {computer_choice}")
 
-if player_choice == computer_choice:
-    print("It is a Draw")
-elif player_choice == rock and computer_choice == scissors:
-    print("You Win")
-elif player_choice == scissors and computer_choice == paper:
-    print("You Win")
-elif player_choice == paper and computer_choice == rock:
-    print("You Win")
+    if player_choice == computer_choice:
+        print("It is a Draw")
+    elif player_choice == rock and computer_choice == scissors:
+        print("You Win")
+    elif player_choice == scissors and computer_choice == paper:
+        print("You Win")
+    elif player_choice == paper and computer_choice == rock:
+        print("You Win")
+    else:
+        print("You Loose")
+
 else:
-    print("You Loose")
+    print("Wrong Input! You Lose")
+
+
+
+
